3d1ffc821327af7a110d76e5b8958618b389a271
Gabrielle Alvim <gabrielle.alvim@hotmail.com>
2022-07-27 18:56:44 -0300

    Create tests for delete and edit product (#18)

--- tests/api/product.api.test.js
+++ tests/api/product.api.test.js
@@ -26,9 +26,56 @@ test.describe.parallel('Product API', () => {
 
   test('retrieves an existing product by its id', async ({ request }) => {
     const id = await getProductId(request, authorization, getProductBody())
-
     const response = await request.get(`/produtos/${id}`)
 
     await expect(response).toBeOK()
+  })
+
+  test('deletes a product successfully', async ({ request }) => {
+    const id = await getProductId(request, authorization, getProductBody())
+    const response = await request.delete(`/produtos/${id}`, {
+      headers: { 'Authorization': authorization }
+    })
+
+    await expect(response).toBeOK()
+  })
+
+  test('fails to delete a product when access token is not provided', async ({ request }) => {
+    const id = await getProductId(request, authorization, getProductBody())
+    const response = await request.delete(`/produtos/${id}`)
+
+    await expect(response.status()).toEqual(401)
+  })
+  
+  test('edits a product successfully', async ({ request }) => { 
+    const id = await getProductId(request, authorization, getProductBody())
+    const editProduct = getProductBody()
+
+    const response = await request.put(`/produtos/${id}`, {
+      data: editProduct, 
+      headers: { 'Authorization': authorization }
+    })
+  
+    await expect(response).toBeOK()
+  })
+
+  test('fails to edit a product if other product with the same name already exists', async ({ request }) => { 
+    const product = getProductBody()
+    await request.post('/produtos', {
+      data: product,
+      headers: { 'Authorization': authorization }
+    })    
+
+    const { nome, ...rest } = getProductBody()
+    const editProduct = {
+      nome: product.nome, 
+      ...rest
+    }
+    const response = await request.put('/produtos/nonExistingId', {
+      data: editProduct,
+      headers: { 'Authorization': authorization }
+    })
+    
+    await expect(response.status()).toEqual(400)
   })
 })
