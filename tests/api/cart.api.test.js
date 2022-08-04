36cef2b92e2c63face254931fec0ed1c0522078f
lucas.fraga <ulucasfraga@gmail.com>
2022-08-03 22:22:10 -0300

    update cart file

--- tests/api/cart.api.test.js
+++ tests/api/cart.api.test.js
@@ -1,5 +1,6 @@
 const { test, expect } = require('@playwright/test')
-const { POST_SUCESS } = require('serverest/src/utils/constants')
+const { POST_SUCESS, DELETE_SUCESS, SEM_CARRINHO, TOKEN_INVALID, ESTOQUE_REABASTECIDO } = require('serverest/src/utils/constants')
+const { StatusCodes } = require('http-status-codes')
 
 const { getAuthToken, getProductBody, getProductId, getCartBody, getCartId } = require('../../lib/helpers')
 
@@ -13,33 +14,35 @@ test.describe.parallel('Carts API', () => {
   test('retrieves the list of carts', async ({ request }) => {
     const response = await request.get('/carrinhos')
 
-    await expect(response).toBeOK()
+    expect(response.status()).toEqual(StatusCodes.OK)
   })
 
   test('retrieves an existing cart by its id', async ({ request }) => {
-    const idProduct = await getProductId(request, authorization, getProductBody())
-    const _id = await getCartId(request, authorization, getCartBody(idProduct))
+    const productId = await getProductId(request, authorization, getProductBody())
+    const cartId = await getCartId(request, authorization, getCartBody(productId))
 
-    const response = await request.get(`/carrinhos/${_id}`)
+    const response = await request.get(`/carrinhos/${cartId}`)
 
-    await expect(response).toBeOK()
+    expect(response.status()).toEqual(StatusCodes.OK)
   })
 
   test('creates a cart successfully', async ({ request }) => {
     const authorization = await getAuthToken(request)
-    const idProduct = await getProductId(request, authorization, getProductBody())
+    const productId = await getProductId(request, authorization, getProductBody())
 
     const response = await request.post('/carrinhos', {
-      data: getCartBody(idProduct),
+      data: getCartBody(productId),
       headers: { 'Authorization': authorization }
     })
-    const responseBody = JSON.parse(await response.text())
 
-    await expect(response).toBeOK()
-    expect(responseBody.message).toEqual(POST_SUCESS)
+    expect(response.status()).toEqual(StatusCodes.CREATED)
+
+    const cart = await response.json()
+
+    expect(cart.message).toEqual(POST_SUCESS)
   })
 
-  test('delete an order successfully', async ({ request }) => {
+  test('deletes an order successfully', async ({ request }) => {
     const idProduct = await getProductId(request, authorization, getProductBody())
     await getCartId(request, authorization, getCartBody(idProduct))
 
@@ -47,38 +50,38 @@ test.describe.parallel('Carts API', () => {
       headers: { 'Authorization': authorization }
     })
 
-    await expect(response).toBeOK()
+    expect(response.status()).toEqual(StatusCodes.OK)
 
-    const responseBody = JSON.parse(await response.text())
+    const cart = await response.json()
 
-    await expect(responseBody.message).toBe('Registro excluído com sucesso')
+    expect(await cart.message).toEqual(DELETE_SUCESS)
   })
 
-  test('delete an order without a cart', async ({ request }) => {
+  test('deletes an order without a cart', async ({ request }) => {
     const response = await request.delete('/carrinhos/concluir-compra', {
       headers: { 'Authorization': authorization }
     })
 
-    await expect(response).toBeOK()
+    expect(response.status()).toEqual(StatusCodes.OK)
 
-    const responseBody = JSON.parse(await response.text())
+    const cart = await response.json()
 
-    await expect(responseBody.message).toBe('Não foi encontrado carrinho para esse usuário')
+    expect(cart.message).toEqual(SEM_CARRINHO)
   })
 
-  test('delete an order without a token', async ({ request }) => {
+  test('fails to delete an order if authorization token is not provided', async ({ request }) => {
     const response = await request.delete('/carrinhos/concluir-compra', {
       headers: { 'Authorization': '' }
     })
 
-    await expect(response).not.toBeOK()
+    expect(response.status()).toEqual(StatusCodes.UNAUTHORIZED)
 
-    const responseBody = JSON.parse(await response.text())
+    const cart = await response.json()
 
-    await expect(responseBody.message).toBe('Token de acesso ausente, inválido, expirado ou usuário do token não existe mais')
+    expect(cart.message).toEqual(TOKEN_INVALID)
   })
 
-  test('cancel an order successfully', async ({ request }) => {
+  test('cancels an order successfully', async ({ request }) => {
     const idProduct = await getProductId(request, authorization, getProductBody())
     await getCartId(request, authorization, getCartBody(idProduct))
 
@@ -86,34 +89,34 @@ test.describe.parallel('Carts API', () => {
       headers: { 'Authorization': authorization }
     })
 
-    await expect(response).toBeOK()
+    expect(response.status()).toEqual(StatusCodes.OK)
 
-    const responseBody = JSON.parse(await response.text())
+    const cart = await response.json()
 
-    await expect(responseBody.message).toBe('Registro excluído com sucesso. Estoque dos produtos reabastecido')
+    expect(cart.message).toEqual(`${DELETE_SUCESS}. ${ESTOQUE_REABASTECIDO}`)
   })
 
-  test('cancel an order without a cart', async ({ request }) => {
+  test('cancels an order without a cart', async ({ request }) => {
     const response = await request.delete('/carrinhos/cancelar-compra', {
       headers: { 'Authorization': authorization }
     })
 
-    await expect(response).toBeOK()
+    expect(response.status()).toEqual(StatusCodes.OK)
 
-    const responseBody = JSON.parse(await response.text())
+    const cart = await response.json()
 
-    await expect(responseBody.message).toBe('Não foi encontrado carrinho para esse usuário')
+    expect(cart.message).toEqual(SEM_CARRINHO)
   })
 
-  test('cancel an order without a token', async ({ request }) => {
+  test('fails to cancel an order if authorization token is not provided', async ({ request }) => {
     const response = await request.delete('/carrinhos/cancelar-compra', {
       headers: { 'Authorization': '' }
     })
 
-    await expect(response).not.toBeOK()
+    expect(response.status()).toEqual(StatusCodes.UNAUTHORIZED)
 
-    const responseBody = JSON.parse(await response.text())
+    const cart = await response.json()
 
-    await expect(responseBody.message).toBe('Token de acesso ausente, inválido, expirado ou usuário do token não existe mais')
+    expect(cart.message).toEqual(TOKEN_INVALID)
   })
 })
